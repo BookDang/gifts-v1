240e02f6abba5e50ce9621a071bcb70baf37f86c
BookDang <sachdang.dev@gmail.com>
2024-12-08 16:57:13 +0700

    Remove unused authentication and user-related entities, DTOs, services, and components.

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -5,8 +5,6 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HealthModule } from '@/health/health.module'
 import { MongooseModule } from '@nestjs/mongoose'
-import { UsersModule } from './users/users.module'
-import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -24,8 +22,6 @@ import { AuthModule } from './auth/auth.module'
       entities: [],
     }),
     HealthModule,
-    UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- nextjs/src/utilities/constants/auth.constant.ts
+++
@@ -1,2 +0,0 @@
-export const AUTH_REDIRECT_QUERY_PARAM = 'redirect'
-export const AUTH_LOGIN_PATH = '/login'

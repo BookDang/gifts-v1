47a4e286da7ba6022f3f65e966c2c42b58a62b49
BookDang <sachdang.dev@gmail.com>
2024-12-10 20:54:44 +0700

    feat: Install @nestjs/swagger into the nestjs project.

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
-import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { useContainer } from 'class-validator'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
   app.useGlobalPipes(new ValidationPipe())
-  app.setGlobalPrefix('api')
+  const config = new DocumentBuilder()
+    // .setTitle('Cats example')
+    // .setDescription('The cats API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
   await app.listen(3008)
 }
 bootstrap()
